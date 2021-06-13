50bf47f856e784fecc5f9938d436e2226af25fee
Yaro <yshmarov@gmail.com>
2021-06-13 18:50:20 +0000

    users index and show

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'static_public#landing_page'
   get 'privacy', to: 'static_public#privacy'
   get 'terms', to: 'static_public#terms'
+
+  resources :users, only: %i[index show]
 end
