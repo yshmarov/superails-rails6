2b9c6e907ce23f917f779d2a832ac7e6843c5db8
Yaro <yshmarov@gmail.com>
2021-06-13 12:14:02 +0000

    add omniauth fields to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,29 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2] 
 
   def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
+    user = User.where(email: access_token.info.email).first
 
     unless user
       user = User.create(
-        email: data['email'],
+        email: access_token.info.email,
         password: Devise.friendly_token[0,20]
       )
     end
+
+    user.provider = access_token.provider
+    user.uid = access_token.uid
+    user.name = access_token.info.name
+    user.image = access_token.info.image
+    user.save
+
     user
+  end
+
+  def username
+    if name?
+      name
+    else
+      email
+    end
   end
 end
