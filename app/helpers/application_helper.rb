a429845a48102bd80905dd57d68c503bffa0635c
Yaro <yshmarov@gmail.com>
2021-06-17 16:53:00 +0000

    active link to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,26 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def active_link_to(name, path)
+    content_tag(:li, class: "#{'active fw-bold' if current_page?(path)} nav-item") do
+      link_to name, path, class: "nav-link"
+    end
+  end 
+  
+  def deep_active_link_to(path)
+    content_tag(:li, class: "#{'active fw-bold' if current_page?(path)} nav-item") do
+      link_to path, class: "nav-link" do
+        yield
+      end
+    end 
+  end 
+  
+  def deep_active_link_to_dropdown_item(path)
+    content_tag(:li) do
+      link_to path, class: "#{'active fw-bold' if current_page?(path)} dropdown-item" do
+        yield
+      end
+    end 
+  end
+
 end
