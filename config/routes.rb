c2ec070780958795465864797180a55942aa461a
Yaro <yshmarov@gmail.com>
2021-06-19 11:20:21 +0000

    stripe billing porta

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :posts
 
   post "checkout/create", to: "checkout#create", as: "checkout_create"
+  post "billing_portal/create", to: "billing_portal#create", as: "billing_portal_create"
   resources :webhooks, only: [:create]
 
   resources :users, only: %i[index show]
