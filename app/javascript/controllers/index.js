f20107cbd9917d9d88d115d89728cb8e3a97cc4b
Yaro <yshmarov@gmail.com>
2021-06-22 19:04:41 +0000

    Stimulus Textarea Autogrow

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,10 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
+import TextareaAutogrow from "stimulus-textarea-autogrow"
+
 const application = Application.start()
 const context = require.context("../controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+application.register("textarea-autogrow", TextareaAutogrow)
