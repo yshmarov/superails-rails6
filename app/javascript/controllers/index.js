2e829e13c96925afba987a11907e20f30bb2b191
Yaro <yshmarov@gmail.com>
2021-06-22 19:24:50 +0000

    Stimulus Character Counter

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,9 +2,11 @@ import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 import TextareaAutogrow from "stimulus-textarea-autogrow"
+import CharacterCounter from "stimulus-character-counter"
 
 const application = Application.start()
 const context = require.context("../controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 
 application.register("textarea-autogrow", TextareaAutogrow)
+application.register("character-counter", CharacterCounter)
