06f9415a840fe30d5e655410fa40aed3578ad2a0
Yaro <yshmarov@gmail.com>
2021-06-22 18:38:06 +0000

    validate length of posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,15 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :body, presence: true
-  
+
+  MAX_TITLE_LENGTH = 140
+  MAX_DESCRIPTION_LENGTH = 200
+  MAX_BODY_LENGTH = 50000
+
+  validates :title, length: { maximum: MAX_TITLE_LENGTH }
+  validates :description, length: { maximum: MAX_DESCRIPTION_LENGTH }
+  validates :body, length: { maximum: MAX_BODY_LENGTH }
+
   scope :free, -> { where(premium: false) }
   scope :premium, -> { where(premium: true) }
 end
