3ff2fadc3ca8173427fd4c637b3b0f7024c8d51a
Yaro <yshmarov@gmail.com>
2021-07-07 09:59:30 +0000

    gem invisible_captcha - fewer bot signups

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, 
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
-                   confirmations: 'users/confirmations'
+                   confirmations: 'users/confirmations',
+                   registrations: 'users/registrations'
                  }
   root 'static_public#landing_page'
   get 'privacy', to: 'static_public#privacy'
