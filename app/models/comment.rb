34cfdd08023ea7a56c5819d64732cd6c6081967e
Yaro <yshmarov@gmail.com>
2021-07-15 11:23:07 +0000

    validate length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
   validates :content, presence: true
+  validates :content, length: { minimum: 5 }
 
   def destroy
     update(deleted_at: Time.zone.now)
