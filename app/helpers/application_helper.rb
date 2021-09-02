aa560a2cf9c5caa1b2795e072355b837a648854c
Yaro <yshmarov@gmail.com>
2021-09-02 19:46:33 +0000

    Rails/ContentTag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,13 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def active_link_to(name, path)
-    content_tag(:li, class: "#{'active fw-bold' if current_page?(path)} nav-item") do
+    tag.li(class: "#{'active fw-bold' if current_page?(path)} nav-item") do
       link_to name, path, class: 'nav-link'
     end
   end
 
   def deep_active_link_to(path)
-    content_tag(:li, class: "#{'active fw-bold' if current_page?(path)} nav-item") do
+    tag.li(class: "#{'active fw-bold' if current_page?(path)} nav-item") do
       link_to path, class: 'nav-link' do
         yield
       end
@@ -18,7 +18,7 @@ module ApplicationHelper
   end
 
   def deep_active_link_to_dropdown_item(path)
-    content_tag(:li) do
+    tag.li() do
       link_to path, class: "#{'active fw-bold' if current_page?(path)} dropdown-item" do
         yield
       end
@@ -32,7 +32,7 @@ module ApplicationHelper
     when false
       badge_color = 'badge bg-danger'
     end
-    content_tag(:span, value, class: badge_color)
+    tag.span(value, class: badge_color)
   end
 
   require 'redcarpet'
