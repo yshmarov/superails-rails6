683e3a6616f7623337c3fb5a3546ec4d2dbc7e3e
Yaro <yshmarov@gmail.com>
2021-09-03 10:08:54 +0000

    Layout/HashAlignment

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -4,13 +4,13 @@ class CheckoutController < ApplicationController
   def create
     @session = Stripe::Checkout::Session.create({
                                                   customer: current_user.stripe_customer_id,
-      success_url: posts_url,
-      cancel_url: pricing_url,
-      payment_method_types: ['card'],
-      line_items: [
+                                                  success_url: posts_url,
+                                                  cancel_url: pricing_url,
+                                                  payment_method_types: ['card'],
+                                                  line_items: [
         { price: params[:price], quantity: 1 }
       ],
-      mode: 'subscription'
+                                                  mode: 'subscription'
                                                 })
     respond_to do |format|
       format.js
