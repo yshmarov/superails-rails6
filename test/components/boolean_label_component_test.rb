bf93ed854a44a8bf90a24a003532b90617418765
Yaro <yshmarov@gmail.com>
2021-09-17 06:51:12 +0000

    install gem view_component, replace boolean_label

---
+++ test/components/boolean_label_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class BooleanLabelComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(BooleanLabelComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
