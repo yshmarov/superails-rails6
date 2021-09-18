1e54ea73606005fc5dd730aaaea9594e4183afe8
Yaro <yshmarov@gmail.com>
2021-09-18 11:44:23 +0000

    user component

---
+++ test/components/user_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class UserComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(UserComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
