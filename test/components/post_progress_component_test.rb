c490e866686ddf517ed5b6a4f1369c69b24d1f1c
Yaro <yshmarov@gmail.com>
2021-09-18 21:30:40 +0000

    post progress component

---
+++ test/components/post_progress_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class PostProgressComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(PostProgressComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
