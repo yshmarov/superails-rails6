a31d5a7c896ef822bc8756ef584ea2eefaeb06b8
Yaro <yshmarov@gmail.com>
2021-09-22 14:29:09 +0000

    avatar component - setting image size

--- app/components/avatar_component.rb
+++ app/components/avatar_component.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class AvatarComponent < ViewComponent::Base
-  def initialize(user:)
+  def initialize(user:, size: nil)
     @user = user
+    @size = size
+  end
+
+  def size
+    return '30x30' unless @size.present?
+    
+    @size
   end
 end
