984354e6d19255886c84d826bcc2871f40d3aa0d
Yaro <yshmarov@gmail.com>
2021-09-22 22:31:47 +0000

    use attr_reader

--- app/components/avatar_component.rb
+++ app/components/avatar_component.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 class AvatarComponent < ViewComponent::Base
+  attr_reader :user, :size, :alt
+
   def initialize(user:, size: nil, alt: "default image alt")
     @user = user
     @size = size
     @alt = alt
   end
 
-  def size
-    return '30x30' unless @size.present?
+  private
+
+  def image_size
+    return '30x30' unless size.present?
     
-    @size
+    size
   end
 end
