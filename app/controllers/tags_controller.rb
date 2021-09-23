54de2bc8001e924d969bcc2cc485efd6116a8758
Yaroslav Shmarov <yshmarov@gmail.com>
2021-09-23 16:48:05 +0200

    add tom select (#26)

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,15 @@
+class TagsController < ApplicationController
+  def create
+    tag = Tag.new(tag_params)
+    if tag.valid?
+      tag.save
+      render json: tag
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+end
