4e852ebd602ac1ce0958e5c8eb004ac3f6eadabd
Yaro <yshmarov@gmail.com>
2021-09-25 21:43:04 +0000

    preview with params

--- test/components/previews/progress_component_preview.rb
+++ test/components/previews/progress_component_preview.rb
@@ -1,6 +1,14 @@
 class Posts::ProgressComponentPreview < ViewComponent::Preview
   # layout false
 
+  def render_boolean(value = false)
+    if value
+      render(BooleanLabelComponent.new(value: true)) { content_tag(:div, true) }
+    else
+      render(BooleanLabelComponent.new(value: false)) { content_tag(:div, false) }
+    end
+  end
+
   def with_votes
     render(Posts::ProgressComponent.new(post: Post.where.not(cached_scoped_like_votes_total: [0, nil]).first))
   end
