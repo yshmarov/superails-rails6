e37371ae45f4e85f63f5ad8069ccad48eeb84c4d
Yaro <yshmarov@gmail.com>
2021-09-25 21:43:35 +0000

    default value boolean label component

--- app/components/boolean_label_component.rb
+++ app/components/boolean_label_component.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
 class BooleanLabelComponent < ViewComponent::Base
-  def initialize(value:)
+  attr_reader :value
+
+  def initialize(value: false)
     @value = value
   end
 
   private
 
   def badge_color
-    case @value
+    case value
     when true
       'badge bg-success'
     when false
       'badge bg-danger'
+    # else not needed if we already have a default in the initialize (value: false)
+    # else
+    #   'badge bg-warning'
     end
   end
 end
