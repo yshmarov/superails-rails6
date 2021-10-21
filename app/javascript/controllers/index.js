9fb21c727c03ded7fab2fdf68df2739faf1733cf
Ahmed Nadar <ahmedfay@hotmail.com>
2021-10-21 11:01:14 +0400

    Remove Webpack and any references to it before install esbuilder and tailwind.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,12 +1,33 @@
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+// import { Application } from "stimulus"
+// import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-import TextareaAutogrow from "stimulus-textarea-autogrow"
-import CharacterCounter from "stimulus-character-counter"
+// import TextareaAutogrow from "stimulus-textarea-autogrow"
+// import CharacterCounter from "stimulus-character-counter"
 
-const application = Application.start()
-const context = require.context("../controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+// const application = Application.start()
+// const context = require.context("../controllers", true, /\.js$/)
+// application.load(definitionsFromContext(context))
 
-application.register("textarea-autogrow", TextareaAutogrow)
-application.register("character-counter", CharacterCounter)
+// application.register("textarea-autogrow", TextareaAutogrow)
+// application.register("character-counter", CharacterCounter)
+
+
+
+import { Application } from "@hotwired/stimulus";
+const application = Application.start();
+
+// Configure Stimulus development experience
+application.warnings = true;
+application.debug = false;
+window.Stimulus = application;
+
+export { application };
+
+import TextareaAutogrow from "stimulus-textarea-autogrow";
+application.register("textarea-autogrow", TextareaAutogrow);
+
+import CharacterCounter from "stimulus-character-counter";
+application.register("character-counter", CharacterCounter);
+
+// import HelloController from "./hello_controller";
+// application.register("hello", HelloController);
